ef319ca7396f97dd58ed616bc5c17896e8ef67c1
Alirio Aranguren <a.aranguren@concentra.com.do>
2019-08-07 23:49:55 -0400

    backend completed

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,10 @@
 |
 */
 
-Route::get('/', function () {
+Route::resource('/', 'WelcomeController');
+/* Route::get('/', function () {
     return view('welcome');
-});
+}); */
 Route::resource('dashboard', 'DashboardController');
 /* Route::get('/dashboard',function(){
     return view('dashboard');
