6edccd0838a5c26223d6b3cf7cb7f26f98cc3858
Alirio Aranguren <a.aranguren@concentra.com.do>
2019-08-09 16:15:25 -0400

    fixed bugs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,15 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Alirio CV</title>
+        <title>Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        
-        {{-- <link rel="stylesheet" href="/css/bootstrap.css"> --}}        
-        <link href="{{ asset('assets/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-        <link rel="stylesheet" href="/css/freelancer.min.css">
         <style>
             html, body {
                 background-color: #fff;
@@ -67,106 +63,25 @@
             }
         </style>
     </head>
-    <body id="page-top">
-            <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-                <div class="container">
-                    <a class="navbar-brand js-scroll-trigger" href="#page-top"></a>
-                    <button class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio">Portfolio</a>
-                        </li>
-                        <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a>
-                        </li>
-                        <li class="nav-item mx-0 mx-lg-1">
-                        <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact">Contact</a>
-                        </li>
-                    </ul>
-                    </div>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
                 </div>
-                {{-- @if (Route::has('login'))
-                    <div class="top-right links">
-                        @auth
-                            <a href="{{ url('/home') }}">Home</a>
-                        @else
-                            <a href="{{ route('login') }}">Login</a>
+            @endif
 
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}">Register</a>
-                            @endif
-                        @endauth
-                    </div>
-                @endif --}}
-            </nav>
-
-            <!-- Masthead -->
-            <header class="masthead bg-primary text-white text-center">
-                    <div class="container d-flex align-items-center flex-column">
-                
-                    <!-- Masthead Avatar Image -->
-                    <img class="masthead-avatar mb-5" src="img/avataaars.svg" alt="">
-                
-                    <!-- Masthead Heading -->
-                    <h1 class="masthead-heading text-uppercase mb-0">Alirio Aranguren</h1>
-                
-                    <!-- Icon Divider -->
-                    <div class="divider-custom divider-light">
-                        <div class="divider-custom-line"></div>
-                        <div class="divider-custom-icon">
-                            <i class="fas fa-star"></i>
-                        </div>
-                        <div class="divider-custom-line"></div>
-                    </div>
-                
-                    <!-- Masthead Subheading -->
-                    <p class="masthead-subheading font-weight-light mb-0">Web Developer</p>
-                
-                    </div>
-            </header>
-
-            <section class="page-section portfolio" id="portfolio">
-                <div class="container">
-                    
-                </div>
-            </section>
-
-            <section id="contact">
-                <div class="content col-lg-8 mx-auto">
-                    <form action="{{route('contacts.store')}}"  method="post">
-                        @csrf
-                        <div class="form-group">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" id="name" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="email" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="phone">Phone</label>
-                            <input type="phone" name="phone" id="phone" class="form-control">
-                        </div>
-                        <div class="form-group">
-                            <label for="content">Text</label>
-                            <textarea name="content" id="content" cols="30" rows="5" class="form-control"></textarea>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-outline-info"><i class="fas fa-envelope"></i> Send</button>
-                        </div>
-                    </form>
-                </div>
-            </section>
-
-            
-
-            {{-- <div class="content">
+            <div class="content">
                 <div class="title m-b-md">
-                    Alirio Aranguren 
+                    Backend CMS Test
                 </div>
 
                 <div class="links">
@@ -178,16 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
-            </div> --}}
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Plugin JavaScript -->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for this template -->
-    <script src="js/freelancer.min.js"></script>
+            </div>
+        </div>
     </body>
 </html>
